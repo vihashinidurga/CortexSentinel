f303c488116fa7fa66e74ea0a32c75b46a1d42c7
Durgavihashini <saldv178@gmail.com>
2026-03-02 05:59:17 +0000

    feat: aesthetic chnages

--- models/intermediate/int_orders_enriched.sql
+++ models/intermediate/int_orders_enriched.sql
@@ -1,33 +1,27 @@
-with orders as (
-    select * from {{ ref('stg_orders') }}
-),
+with
+    orders as (select * from {{ ref("stg_orders") }}),
 
-customers as (
-    select * from {{ ref('stg_customers') }}
-),
+    customers as (select * from {{ ref("stg_customers") }}),
 
-nations as (
-    select * from {{ ref('stg_nation') }}
-),
+    nations as (select * from {{ ref("stg_nation") }}),
 
-regions as (
-    select * from {{ ref('stg_region') }}
-),
+    regions as (select * from {{ ref("stg_region") }}),
 
-enriched as (
-    select
-        o.order_key,
-        o.order_id,
-        o.order_date,
-        o.status as order_status,
-        c.customer_id,
-        c.name as customer_name,
-        n.name as nation_name,
-        r.name as region_name
-    from orders o
-    left join customers c on o.customer_key = c.customer_id
-    left join nations n on c.nation_key = n.nation_id
-    left join regions r on n.region_key = r.region_id
-)
+    enriched as (
+        select
+            o.order_key,
+            o.order_id,
+            o.order_date,
+            o.status as order_status,
+            c.customer_id,
+            c.name as customer_name,
+            n.name as nation_name,
+            r.name as region_name
+        from orders o
+        left join customers c on o.customer_key = c.customer_id
+        left join nations n on c.nation_key = n.nation_id
+        left join regions r on n.region_key = r.region_id
+    )
 
-select * from enriched
+select *
+from enriched
--- models/marts/mart_revenue_by_region_year.sql
+++ models/marts/mart_revenue_by_region_year.sql
@@ -1,22 +1,20 @@
-with orders as (
-    select * from {{ ref('int_orders_enriched') }}
-),
+with
+    orders as (select * from {{ ref("int_orders_enriched") }}),
 
-lineitem as (
-    select * from {{ ref('int_lineitem_enriched') }}
-),
+    lineitem as (select * from {{ ref("int_lineitem_enriched") }}),
 
-revenue_aggregation as (
-    select
-        o.region_name,
-        o.nation_name,
-        -- {{ date_part('year', 'o.order_date') }} as order_year,
-        date_part('year', 'o.order_date') as order_year,
-        sum(l.extended_price * (1 - l.discount_percentage)) as total_revenue
-    from lineitem l
-    inner join orders o on l.order_key = o.order_key
-    group by 1, 2, 3
-)
+    revenue_aggregation as (
+        select
+            o.region_name,
+            o.nation_name,
+            -- {{ date_part('year', 'o.order_date') }} as order_year,
+            date_part('year', 'o.order_date') as order_year,
+            sum(l.extended_price * (1 - l.discount_percentage)) as total_revenue
+        from lineitem l
+        inner join orders o on l.order_key = o.order_key
+        group by 1, 2, 3
+    )
 
-select * from revenue_aggregation
+select *
+from revenue_aggregation
 order by region_name, nation_name, order_year
