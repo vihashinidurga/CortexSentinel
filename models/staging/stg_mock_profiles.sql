df84fd566940ffbfe799bbc1db2d6e8e0420e8a2
Durgavihashini <saldv178@gmail.com>
2026-03-03 06:15:54 +0000

    feat: created validation files

---
+++ models/staging/stg_mock_profiles.sql
@@ -0,0 +1,5 @@
+select 'Professional Architect' as job_title, 'Antigravity' as company
+union all
+select 'Astronaut at Pizza Hut' as job_title, 'Moon Corp' as company
+union all
+select 'Data Scientist' as job_title, 'Relanto' as company
